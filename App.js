4f2a14a6076ca86c55917b26242e76cb881f0c6f
sck42 <sck42@pitt.edu>
2022-03-05 14:16:49 -0500

    unimportant test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Made this change</Text>
       <StatusBar style="auto" />
     </View>
   );
