9ed6bfb8404b082d001845a88d5b62c8742efa96
sck42 <sck42@pitt.edu>
2022-03-06 15:28:01 -0500

    Added Details screen and fixed more routing. Issues with done screen

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { SectionList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Button, SectionList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator, HeaderBackButton } from '@react-navigation/native-stack';
 import { skills } from './skills.json'; 
+import React, {useLayoutEffect} from 'react'; 
+
 
 const Stack = createNativeStackNavigator();
 
 function HomeScreen({ navigation }) {
   let quiz = async () => {
-    navigation.navigate("Question");
+    navigation.navigate("Question", {
+      questionNum: 1,
+      answer: "",
+      path: []
+    });
   }
 
   let list = () => {
@@ -35,19 +41,25 @@ function HomeScreen({ navigation }) {
   )
 }
 
-function ListScreen() {
+function ListScreen({navigation}) {
   return (
-    // <FlatList
-    //   contentContainerStyle={styles.lstContainer}
-    //   data={skills}
-    //   renderItem={({ item }) => <Text style={styles.row}>{item.name}</Text>}
-    //   keyExtractor={( item ) => item.id}
-    // />
     <View style={{ height: "100%" }}>
       <SectionList
-        style={styles.lstContainer}
+        style={lstStyles.lstContainer}
         sections={skills}
-        renderItem={({ item }) => <Text style={styles.row}>{item.name}</Text>}
+        renderItem={({ item, section }) => (
+          <TouchableOpacity 
+            key={item.id} 
+            onPress={() => {
+              navigation.navigate("ListDetail", {
+                sectionId: section.id,
+                itemId: item.id
+              })
+            }}
+          >
+            <Text style={styles.row}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
         renderSectionHeader={({ section }) => (
           <Text style={styles.header}>{section.title}</Text>
         )}
@@ -57,9 +69,73 @@ function ListScreen() {
   );
 }
 
-function QuestionScreen() {
+function filterById(jsonObject, id) {
+  return jsonObject.filter(function(jsonObject) {
+    return (jsonObject['id'] == id);
+  })[0];
+}
+
+
+function ListDetailScreen({route, navigation}) {
+  let itemParams = route.params; 
+  let section = filterById(skills, itemParams.sectionId); 
+  let item = filterById(section.data, itemParams.itemId); 
   return (
-    <View></View>
+    <View style={{ height: "100%", alignContent: "center", justifyContent: "center" }}>
+      <Text style={lstStyles.lstDetailHeader}>{section.title}: {item.name}</Text>
+      <View style={{ height: "90%"}}>
+        <View style={{height: "50%"}}>
+          <Text style={lstStyles.lstDetailRecord}>Description: {item.description}</Text>
+        </View>
+        <View style={{height: "50%"}}>
+          <Text style={lstStyles.lstDetailRecord}>Example: {item.example}</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+class DoneScreen extends React.Component {
+  static navigationOptions = () => {
+    return{
+      headerLeft:(<HeaderBackButton onPress={()=>{navigation.navigate("Welcome")}}/>)
+    }
+  };
+
+  render() {
+    return (
+      <View>
+        <Text style={{color: '#888', fontSize: 35, textAlign: 'center'}}>Suggested Skills to Practice</Text>
+      </View>
+    )
+  };
+}
+
+function QuestionScreen({route, navigation}) {
+  let question = route.params;  
+
+  let nextQuestion = () => {
+    if(question.questionNum >= 5){
+      navigation.navigate("Done", {
+        questionNum: question.questionNum+1,
+        answer: "answer question 5",
+        path: "test"
+      });
+    } else {
+      navigation.navigate("Question", {
+        questionNum: question.questionNum+1,
+        answer: "answer question " + question.questionNum,
+        path: "test"
+      });
+    }    
+  }
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Question {question.questionNum}</Text>
+      <TouchableOpacity onPress={nextQuestion} style={styles.button}>
+        <Text>Next Question</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
@@ -70,15 +146,14 @@ export default function App() {
         <Stack.Screen name="Welcome" component={HomeScreen} />
         <Stack.Screen name="List" component={ListScreen} />
         <Stack.Screen name="Question" component={QuestionScreen} />
+        <Stack.Screen name="ListDetail" component={ListDetailScreen} />
+        <Stack.Screen name="Done" component={DoneScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  lstContainer: {
-    flex:1,
-  },
   container: {
     flex: 1,
     flexDirection: 'column',
@@ -112,5 +187,50 @@ const styles = StyleSheet.create({
     backgroundColor: 'steelblue',
     color: 'white',
     fontWeight: 'bold',
+  },
+  viewPager: {
+    flex: 1,
+  },
+  page: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const lstStyles = StyleSheet.create({
+  lstContainer: {
+    flex:1,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  row: {
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: 'skyblue',
+  },
+  header: {
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: 'steelblue',
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  lstDetailHeader: {
+    padding: 15,
+    marginBottom: 5,
+    fontWeight: 'bold',
+    color: '#888', 
+    fontSize: 30, 
+    textAlign: 'center'
+  },
+  lstDetailRecord: {
+    padding: 15,
+    marginBottom: 5,
+    color: '#888', 
+    fontSize: 20, 
   },
 });
